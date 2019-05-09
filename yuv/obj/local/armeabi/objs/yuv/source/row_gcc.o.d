787fba7ec2e1df2aa1e3daf06b0f5b9d7f7ab976
haitao.li <haitao.li@bkjk.com>
2019-05-09 20:50:08 +0800

    APOLLO-425:添加libyuv

---
+++ yuv/obj/local/armeabi/objs/yuv/source/row_gcc.o.d
@@ -0,0 +1,53 @@
+/Users/bkjk/Downloads/obj/local/armeabi/objs/yuv/source/row_gcc.o: \
+  /Users/bkjk/Downloads/jni/source/row_gcc.cc \
+  /Users/bkjk/Downloads/jni/include/libyuv/row.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stdlib.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+  /Users/bkjk/Downloads/jni/include/libyuv/basic_types.h \
+  /Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stddef.h
+
+/Users/bkjk/Downloads/jni/include/libyuv/row.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/Users/bkjk/Downloads/jni/include/libyuv/basic_types.h:
+
+/Users/bkjk/ndk/android-ndk-r14b/sources/cxx-stl/stlport/stlport/stddef.h:
